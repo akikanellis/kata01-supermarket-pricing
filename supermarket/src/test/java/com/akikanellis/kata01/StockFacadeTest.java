a932dd68e55f679d451c78c1a190dcd5a94c8274
Aki Kanellis <me@akikanellis.com>
2016-11-06 11:51:52 +0000

    Add stock facade

---
+++ supermarket/src/test/java/com/akikanellis/kata01/StockFacadeTest.java
@@ -0,0 +1,11 @@
+package com.akikanellis.kata01;
+
+import org.junit.Before;
+
+public class StockFacadeTest {
+    private StockFacade stockFacade;
+
+    @Before public void beforeEach() { stockFacade = new StockFacade(); }
+
+
+}
