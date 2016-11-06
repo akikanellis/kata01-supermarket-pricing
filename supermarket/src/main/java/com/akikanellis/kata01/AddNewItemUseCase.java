f7f1ff2840b8f39da7c9dc856b2149d89d00a0ef
Aki Kanellis <me@akikanellis.com>
2016-11-06 14:08:18 +0000

    Create AddNewItemUseCase

---
+++ supermarket/src/main/java/com/akikanellis/kata01/AddNewItemUseCase.java
@@ -0,0 +1,8 @@
+package com.akikanellis.kata01;
+
+public class AddNewItemUseCase {
+
+    public void execute(Item item) {
+        throw new UnsupportedOperationException("To be implemented.");
+    }
+}
