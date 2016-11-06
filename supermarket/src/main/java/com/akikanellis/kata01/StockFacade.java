2662203bbc343cdfd4804c02ba84352300869d2a
Aki Kanellis <me@akikanellis.com>
2016-11-06 14:13:08 +0000

    Add new item from facade with use case

--- supermarket/src/main/java/com/akikanellis/kata01/StockFacade.java
+++ supermarket/src/main/java/com/akikanellis/kata01/StockFacade.java
@@ -1,4 +1,11 @@
 package com.akikanellis.kata01;
 
 public class StockFacade {
+    private final AddNewItemUseCase addNewItemUseCase;
+
+    public StockFacade(AddNewItemUseCase addNewItemUseCase) {
+        this.addNewItemUseCase = addNewItemUseCase;
+    }
+
+    public void addNewItem(Item item) { addNewItemUseCase.execute(item); }
 }
--- supermarket/src/test/java/com/akikanellis/kata01/StockFacadeTest.java
+++ supermarket/src/test/java/com/akikanellis/kata01/StockFacadeTest.java
@@ -1,11 +1,29 @@
 package com.akikanellis.kata01;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
 public class StockFacadeTest {
+    @Mock private AddNewItemUseCase addNewItemUseCase;
     private StockFacade stockFacade;
 
-    @Before public void beforeEach() { stockFacade = new StockFacade(); }
+    @Before public void beforeEach() { stockFacade = new StockFacade(addNewItemUseCase); }
 
+    @Test public void addingNewItem_usesUseCase() {
+        Item item = Item.builder()
+                .barcode(1)
+                .name("Apple")
+                .price(Price.ONE)
+                .build();
 
+        stockFacade.addNewItem(item);
+
+        verify(addNewItemUseCase).execute(item);
+    }
 }
