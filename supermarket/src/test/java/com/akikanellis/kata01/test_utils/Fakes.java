1316467fc43e954316599117a27ccda43e441900
Aki Kanellis <me@akikanellis.com>
2016-11-06 17:06:59 +0000

    Make into utility class

--- supermarket/src/test/java/com/akikanellis/kata01/test_utils/Fakes.java
+++ supermarket/src/test/java/com/akikanellis/kata01/test_utils/Fakes.java
@@ -3,7 +3,9 @@ package com.akikanellis.kata01.test_utils;
 import com.akikanellis.kata01.Item;
 import com.akikanellis.kata01.Price;
 
-public class Fakes {
+public final class Fakes {
+
+    private Fakes() { throw new AssertionError("No instances."); }
 
     public static Item createDefaultItem() {
         return Item.builder()
