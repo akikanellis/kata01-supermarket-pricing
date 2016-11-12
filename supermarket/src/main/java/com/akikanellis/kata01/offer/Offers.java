e64c729ac45f6160b62527fe9c2cc18e92cb4c6d
Aki Kanellis <me@akikanellis.com>
2016-11-12 18:59:23 +0000

    Remove unused fromEntries

--- supermarket/src/main/java/com/akikanellis/kata01/offer/Offers.java
+++ supermarket/src/main/java/com/akikanellis/kata01/offer/Offers.java
@@ -3,9 +3,6 @@ package com.akikanellis.kata01.offer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public final class Offers {
     private final List<OfferWithQuantity> offersWithQuantities;
@@ -13,14 +10,6 @@ public final class Offers {
     private Offers(List<OfferWithQuantity> offersWithQuantities) { this.offersWithQuantities = offersWithQuantities; }
 
     public static Offers fromList(List<OfferWithQuantity> itemsWithQuantities) {
-        return new Offers(itemsWithQuantities);
-    }
-
-    public static Offers fromEntries(Set<Map.Entry<Offer, Integer>> entries) {
-        List<OfferWithQuantity> itemsWithQuantities = entries.stream()
-                .map(entry -> OfferWithQuantity.create(entry.getKey(), entry.getValue()))
-                .collect(Collectors.toList());
-
         return new Offers(itemsWithQuantities);
     }
 
