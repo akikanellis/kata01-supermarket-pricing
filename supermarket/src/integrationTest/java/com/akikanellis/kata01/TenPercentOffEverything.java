57e1bfc7baa73a09152f5db8af48da9eae77d8ea
Aki Kanellis <me@akikanellis.com>
2016-11-13 18:12:48 +0000

    Refactor ten percent off everything strategy

--- supermarket/src/integrationTest/java/com/akikanellis/kata01/TenPercentOffEverything.java
+++ supermarket/src/integrationTest/java/com/akikanellis/kata01/TenPercentOffEverything.java
@@ -8,22 +8,30 @@ import com.akikanellis.kata01.offer.Offers;
 import com.akikanellis.kata01.offer.QuantifiedOffer;
 import com.akikanellis.kata01.price.Price;
 
+/**
+ * An example offer strategy used for testing where all of our items are discounted by 10%.
+ */
 public class TenPercentOffEverything extends OfferStrategy {
+    private static final double TEN_PERCENT = 0.10;
 
     private TenPercentOffEverything(long id) { super(id, "10% off everything"); }
 
     public static OfferStrategy create(int id) { return new TenPercentOffEverything(id); }
 
     @Override public Offers calculateOffers(Items items) {
-        Price itemsPrice = items.stream()
-                .map(QuantifiedItem::totalPrice)
-                .reduce(Price::add)
-                .orElse(Price.ZERO);
+        Price itemsTotalPrice = calculateItemsTotalPrice(items);
+        Price discount = itemsTotalPrice.multiplyBy(TEN_PERCENT).negate();
 
-        Price discount = itemsPrice.multiplyBy(0.10).negate();
         Offer offer = Offer.create(description(), discount);
         QuantifiedOffer quantifiedOffer = QuantifiedOffer.create(offer, 1);
 
         return Offers.fromSingle(quantifiedOffer);
+    }
+
+    private Price calculateItemsTotalPrice(Items items) {
+        return items.stream()
+                .map(QuantifiedItem::totalPrice)
+                .reduce(Price::add)
+                .orElse(Price.ZERO);
     }
 }
